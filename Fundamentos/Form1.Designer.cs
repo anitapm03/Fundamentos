f215340f3651654ba468dbb1e515bf249c943cd8
anitapm03 <apereiramolinero@gmail.com>
2023-12-18 12:35:43 +0100

    Add project files.

---
+++ Fundamentos/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Fundamentos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnPulsar = new Button();
+            txtNombre = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnPulsar
+            // 
+            btnPulsar.Location = new Point(29, 94);
+            btnPulsar.Name = "btnPulsar";
+            btnPulsar.Size = new Size(75, 23);
+            btnPulsar.TabIndex = 0;
+            btnPulsar.Text = "Pulsar";
+            btnPulsar.UseVisualStyleBackColor = true;
+            btnPulsar.Click += btnPulsar_Click_1;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(29, 47);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Nombre: ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(282, 211);
+            Controls.Add(label1);
+            Controls.Add(txtNombre);
+            Controls.Add(btnPulsar);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnPulsar;
+        private TextBox txtNombre;
+        private Label label1;
+    }
+}
---
+++ Fundamentos/Form1.cs
@@ -0,0 +1,34 @@
+namespace Fundamentos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPulsar_Click_1(object sender, EventArgs e)
+        {
+            //solo escribiremos aqui 
+            this.txtNombre.Location = new Point(38, 10);
+
+            //tenemos propiedades de tipos de datos primitivos
+            //los primitivos directamente se escriben 
+
+            this.txtNombre.Text = "Soy un texto";
+            this.txtNombre.Width = 180;
+
+            //Esto es una excepción en la norma
+            //tener una clase o estructura final
+            //no se instancia, se utilza.
+            //por ejemplo, la clase color
+
+            this.BackColor = Color.Green;
+
+            //PONGAMOS QUE QUEREMOS ALINEAR EL 
+            //TEXTO DE UNA CAJA
+            this.txtNombre.TextAlign = HorizontalAlignment.Right;
+
+        }
+    }
+}
