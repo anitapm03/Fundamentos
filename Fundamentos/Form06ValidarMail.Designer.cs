f8e44e79fcf755b025f522ab999e04ee1f6433e4
anitapm03 <apereiramolinero@gmail.com>
2023-12-19 13:38:38 +0100

    strings sin acabar

---
+++ Fundamentos/Form06ValidarMail.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Fundamentos
+{
+    partial class Form06ValidarMail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtmail = new TextBox();
+            btnValidar = new Button();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(181, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Introduce un email para validarlo";
+            // 
+            // txtmail
+            // 
+            txtmail.Location = new Point(30, 68);
+            txtmail.Name = "txtmail";
+            txtmail.Size = new Size(191, 23);
+            txtmail.TabIndex = 1;
+            // 
+            // btnValidar
+            // 
+            btnValidar.Location = new Point(30, 107);
+            btnValidar.Name = "btnValidar";
+            btnValidar.Size = new Size(75, 23);
+            btnValidar.TabIndex = 2;
+            btnValidar.Text = "Validar!";
+            btnValidar.UseVisualStyleBackColor = true;
+            btnValidar.Click += btnValidar_Click;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(88, 155);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(56, 15);
+            lblResultado.TabIndex = 3;
+            lblResultado.Text = "resultado";
+            // 
+            // Form06ValidarMail
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResultado);
+            Controls.Add(btnValidar);
+            Controls.Add(txtmail);
+            Controls.Add(label1);
+            Name = "Form06ValidarMail";
+            Text = "Form06ValidarMail";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtmail;
+        private Button btnValidar;
+        private Label lblResultado;
+    }
+}
---
+++ Fundamentos/Form06ValidarMail.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos
+{
+    public partial class Form06ValidarMail : Form
+    {
+        public Form06ValidarMail()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            string mail = txtmail.Text;
+            int longitud = mail.Length;
+            bool valido1 = false;
+            bool valido2 = false;
+            bool valido3 = false;
+            bool valido4 = false;
+            bool valido5 = false;
+            bool valido6 = false;
+
+            if (mail.Contains('@') == true)
+            {
+                valido1 = true;
+            }
+            else
+            {
+                valido1 = false;
+            }
+
+            if (mail.Contains('.') == true)
+            {
+                valido2 = true;
+
+                int posPunto = mail.LastIndexOf('.');
+
+                int posDom = longitud - posPunto;
+
+                string dom = mail.Substring(posDom);
+
+                this.lblResultado.Text = dom;
+
+            } else
+            {
+                valido2 = false;
+            }
+
+            if (mail.StartsWith("@") == false || mail.EndsWith("@") == false)
+            {
+                valido3 = true;
+            } 
+            else
+            {
+                valido3 = false;
+            }
+
+            int contadorArrobas = mail.Count(c => c == '@');
+            if (contadorArrobas != 1)
+            {
+                valido4 = false; // Más de una @
+            }
+
+            this.lblResultado.Text = "No es válido";
+        }
+
+        static bool ValidarMail(string correo)
+        {
+            
+
+            // No más de una @
+            int contadorArrobas = correo.Count(c => c == '@');
+            if (contadorArrobas != 1)
+            {
+                return false; // Más de una @
+            }
+
+            // Exista un punto
+            if (!correo.Contains("."))
+            {
+                return false; // No hay punto
+            }
+
+            // Exista un punto después de @
+            int posicionArroba = correo.IndexOf('@');
+            if (!correo.Substring(posicionArroba).Contains("."))
+            {
+                return false; // No hay punto después de @
+            }
+
+            // Dominio de 2 a 4 caracteres
+            string dominio = correo.Substring(correo.LastIndexOf('.'));
+            if (dominio.Length < 3 || dominio.Length > 5)
+            {
+                return false; // Dominio no tiene entre 2 y 4 caracteres
+            }
+
+            // Si pasa todas las validaciones, es un correo válido
+            return true;
+        }
+    }
+}
